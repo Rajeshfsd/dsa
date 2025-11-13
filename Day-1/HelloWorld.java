32e975a1e77d18bf973e14a48020f719ac8314bb
Rajeshfsd <nrajesh.nadakuditi@gmail.com>
2025-11-14 04:45:55 +0530

    Day 1

---
+++ Day-1/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld
+{
+    public static void main(String [] args)
+    {
+        String fullName="Mahesh Babu";
+        System.out.println("Hello World!");
+        
+        System.out.println(fullName);
+    }
+}
